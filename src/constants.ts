a38c0a39ace7b29f69e517eb1b1505ee5783e286
Edzard <edzard.potgieter@gmail.com>
2026-04-03 13:21:15 +0200

    feat: add SUBDISTRICT_HIERARCHY constant and Region type definition

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const SUBDISTRICT_HIERARCHY = {
-  'Rural East': ['George', 'Oudtshoorn', 'CKD', 'Knysna'],
-  'Rural West': ['Saldana', 'Bergrivier', 'Matzikamma'],
-  'Rural Central': ['Drakenstein', 'Witzenberg', 'Aghallas', 'Swellendam']
+  'Rural East': ['CKD', 'Knysna', 'George', 'Oudtshoorn', 'Mosselbay', 'Hessequa'],
+  'Rural Central': ['Witzenberg', 'Breede Valley', 'Langeberg', 'Swellendam', 'Cape Agulhas', 'Overstrand', 'Theewaterskloof'],
+  'Rural West': ['Stellenbosch', 'Drakenstein', 'Swartland', 'Saldanha', 'Bergrivier', 'Cederberg', 'Matzikamma']
 } as const;
 
 export type Region = keyof typeof SUBDISTRICT_HIERARCHY;
